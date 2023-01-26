51e92f5bbda875a66dcf601af8ed8e6cf9dba8d1
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2023-01-26 01:57:48 +0100

    updates

--- .chezmoitemplates/install.global.sh
+++ .chezmoitemplates/install.global.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
+# TODO fix auto oh-my-zsh install
 # Install Oh my zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
 	echo "Installing Oh my zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# TODO fix auto plugin install
 # Install zsh syntax highlighting plugin
 if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
 	echo "Installing zsh syntax highlighting plugin"
--- .chezmoitemplates/install.linux.sh
+++ .chezmoitemplates/install.linux.sh
@@ -9,8 +9,10 @@ install() {
 
 echo "Installing APT packages:"
 # List of apt packages to install
-APT_PACKAGES="git curl wget zsh exuberant-tags tmux npm python3 python3-pip bear gdu lazygit build-essential"
+APT_PACKAGES="git curl wget zsh exuberant-ctags tmux npm python3 python3-pip bear build-essential cmake"
 # Loop to install all wanted packages
 for PACKAGE in $APT_PACKAGES; do
 	install $PACKAGE;
 done
+
+# TODO install gdu and lazygit
