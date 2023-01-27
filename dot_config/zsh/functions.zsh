6a5e7ccef052fa065c2bb67d8ffd3d9d5aba9215
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2023-01-27 15:49:53 +0100

    udate keygen function

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -60,7 +60,7 @@ function dval {
 	docker run --rm --workdir $HOME -v $PWD:$HOME -it arthurpigeon/valgrind
 }
 
-function keygen {
+function old_keygen {
 	if [ $# != 2 ]; then
 		echo "Usage: $0 [key_name] [email_address]"
 	else
@@ -75,4 +75,82 @@ function keygen {
 		echo "The public key:"
 		cat ~/.ssh/$1_ed.pub
 	fi
+}
+
+function keygen() {
+  if [ ! -x "$(command -v gum)" ]; then
+    echo "Error: need gum tool to run this script, install here (https://github.com/charmbracelet/gum)"
+    return 1
+  fi
+
+  echo "Choose your algorithm: "
+  local SSH_ALGO=$(gum choose ed25519 ecdsa rsa)
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+
+  local PROMPT_GUM="Enter your email: "
+  local EMAIL_GUM=$(gum input --prompt "$PROMPT_GUM" --placeholder "john.davis@gmail.com")
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+
+  local DEFAULT_LOCATION="$HOME/.ssh"
+  local PROMPT_GUM="Choose where to store the key (enter for default): "
+  local LOCATION=$(gum input --prompt "$PROMPT_GUM" --placeholder "$DEFAULT_LOCATION")
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+  if [ -z "$LOCATION" ]; then
+    local LOCATION="$DEFAULT_LOCATION"
+  fi
+  if [ ! -d "$LOCATION" ]; then
+    echo "Error: '$LOCATION' is not a valid directory"
+    return 1
+  fi
+
+  local PROMPT_GUM="Choose a name for the key: "
+  local NAME_GUM=$(gum input --prompt "$PROMPT_GUM" --placeholder "name")
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+  if [ -z "$NAME_GUM" ]; then
+    echo "Error: please give a name"
+    return 1
+  fi
+
+  local PROMPT_GUM="Give the hostname for the key: "
+  local HOST_GUM=$(gum input --prompt "$PROMPT_GUM" --placeholder "github.com")
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+  if [ -z "$HOST_GUM" ]; then
+    local HOST_GUM="github.com"
+  fi
+
+  local PROMPT_GUM="Give the username for the key: "
+  local USER_GUM=$(gum input --prompt "$PROMPT_GUM" --placeholder "git")
+	if [ "$?" -ne 0 ]; then
+		return 1;
+	fi
+  if [ -z "$USER_GUM" ]; then
+    local USER_GUM="git"
+  fi
+
+  local FILENAME="$LOCATION/$NAME_GUM"_"$SSH_ALGO"
+  echo "Summary:"
+  echo "- Algo: $SSH_ALGO"
+  echo "- Email: $EMAIL_GUM"
+  echo "- Host: $HOST_GUM"
+  echo "- User: $USER_GUM"
+  echo "- File: $FILENAME"
+
+  gum confirm || return 1
+
+  ssh-keygen -t "$SSH_ALGO" -f "$FILENAME" -C "$EMAIL_GUM" -N "" -q || (echo "Error: ssh-keygen failed" && exit 1) || return 1
+  echo 'eval $(ssh-agent -S)'
+  echo "\nHost $NAME_GUM\n\tHostName $HOST_GUM\n\tUser $USER_GUM\n\tAddKeysToAgent yes\n\tIdentityFile $FILENAME" >> "$DEFAULT_LOCATION/config"
+
+  echo '\nYour new public key:'
+  cat "$FILENAME.pub"
 }
