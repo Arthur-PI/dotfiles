3d55ed617d821ccc5c635323847f3f74a3b400dd
Arthur <pigeon.arthur@hotmail.fr>
2023-08-24 14:08:29 +0200

    updates

--- .chezmoitemplates/install.mac.sh
+++ .chezmoitemplates/install.mac.sh
@@ -9,7 +9,7 @@ install() {
 
 echo "Installing Homebrew packages:"
 # List of homebrew packages to install
-BREW_PACKAGES="git wget curl zsh ctags tmux npm python3 bear lazygit gdu sshs neovim"
+BREW_PACKAGES="git wget curl zsh python3 lazygit gdu sshs neovim"
 # Loop to install all wanted packages
 for PACKAGE in $BREW_PACKAGES; do
   install $PACKAGE;
